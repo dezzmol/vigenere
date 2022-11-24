51324df42b5ba09ce5bc406b825e2a16199b439c
dezzmol <105000543+dezzmol@users.noreply.github.com>
2022-11-24 18:37:04 +0400

    Add files via upload

---
+++ Vigenere/Vigenere/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Vigenere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(473, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(473, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Сообщение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ключ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Итоговое сообщение";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(473, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Ввести данные с файла";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 262);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Шифр Виженера";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Vigenere/Vigenere/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Vigenere
+{
+    public partial class Form1 : Form
+    {
+        string[] text;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ReadFile()
+        {
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string name = openFileDialog1.FileName;
+                    text = File.ReadAllLines(name, Encoding.GetEncoding(1251));
+                    
+                    textBox1.Text = text[0];
+                    textBox2.Text = text[1];
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка");
+                }
+            }
+        }
+
+        private void FileCheck(string property)
+        {
+            if (File.Exists("Output.txt") == false)
+            {
+                File.Create("Output.txt");
+                SaveFile(property);
+            }
+            else
+            {
+                SaveFile(property);
+            }
+        }
+
+        private void SaveFile(string property)
+        {
+            StreamWriter sw = new StreamWriter("Output.txt", true);
+            sw.WriteLine(property);
+            sw.WriteLine(textBox1.Text);
+            sw.WriteLine("Key: " + textBox2.Text);
+            sw.WriteLine(textBox3.Text);
+            sw.WriteLine("\n");
+            sw.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ReadFile();
+        }
+
+        private string InputPassword()
+        {
+            if (textBox2.Text == "" || textBox2.Text.Length < 5)
+            {
+                return "error";
+            }
+            else
+            {
+                string s = textBox2.Text;
+                return s;
+            }
+        }
+
+        private string InputMessage()
+        {
+            if (textBox1.Text == "" || textBox1.Text.Length < 5)
+            {
+                return "error";
+            }
+            else
+            {
+                string s = textBox1.Text;
+                return s;
+            }
+        }
+
+        private void ClearTextBox(TextBox textBox)
+        {
+            textBox.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearTextBox(textBox3);
+            string m = InputMessage();
+            string k = InputPassword();
+
+            if (m == "error")
+            {
+                MessageBox.Show("Введите ключ");
+                ClearTextBox(textBox1);
+                return;
+            }
+
+            if (k == "error")
+            {
+                MessageBox.Show("Введите сообщение");
+                ClearTextBox(textBox2);
+                return;
+            }
+
+            Vigenere vigenere = new Vigenere();
+            textBox3.Text = vigenere.Encrypt(m, k);
+            FileCheck("\nЗашифровать");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearTextBox(textBox3);
+            string m = InputMessage();
+            string k = InputPassword();
+
+            if (m == "error")
+            {
+                MessageBox.Show("Введите ключ");
+                ClearTextBox(textBox1);
+                return;
+            }
+
+            if (k == "error")
+            {
+                MessageBox.Show("Введите сообщение");
+                ClearTextBox(textBox2);
+                return;
+            }
+
+            Vigenere vigenere = new Vigenere();
+            textBox3.Text = vigenere.Decrypt(m, k);
+            FileCheck("\nРасшифровать");
+        }
+    }
+
+    public class Vigenere
+    {
+        string A = "абвгдеёжзийклмнопрстуфхцчщщъыьэюя";
+        string letters;
+        
+        private string GetRepeatKey(string s, int n)
+        {
+            var p = s;
+            while (p.Length < n)
+                p += p;
+
+            return p.Substring(0, n);
+        }
+
+        private string VigenereCode(string text, string password, bool encrypting = true)
+        {
+            var repeatKey = GetRepeatKey(password, text.Length);
+            var retValue = "";
+            var q = A.Length;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var letterIndex = A.IndexOf(text[i]);
+                var codeIndex = A.IndexOf(repeatKey[i]);
+                if (letterIndex < 0)
+                {
+                    retValue += text[i].ToString();
+                }
+                else
+                {
+                    retValue += A[(q + letterIndex + ((encrypting ? 1 : -1) * codeIndex)) % q].ToString();
+                }
+            }
+
+            return retValue;
+        }
+
+        public string Encrypt(string Message, string password) => VigenereCode(Message, password);
+
+        public string Decrypt(string Message, string password) => VigenereCode(Message, password, false);
+
+
+    }
+}
